47029228f023396c0655d267324cf17bc2a1aa8b
Ryan McKeever <rkfmckee@gmail.com>
2025-05-11 12:31:00 +0100

    Extract more basic details

---
+++ client/src/components/characterSheet/CharacterSheetBasicDetails.tsx
@@ -0,0 +1,22 @@
+import { ActorSheetData } from "@/schemas/characterSheetSchema";
+import { Heading, Stack } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react/box";
+
+interface CharacterSheetBasicDetailsProps {
+    sheet: ActorSheetData;
+}
+
+const CharacterSheetBasicDetails = ({ sheet }: CharacterSheetBasicDetailsProps) => {
+    const classes = sheet.items.filter((i) => i.type == "class"); //.sort((a, b) => ((a.system.levels ?? 0) > (b.system.levels ?? 0) ? -1 : 1));
+
+    return (
+        <Box className="character-sheet__panel">
+            <Stack>
+                <Heading size="xl">{sheet.name}</Heading>
+                <Heading size="md">{classes.map((c) => `${c.name} ${c.system.levels}, `)}</Heading>
+            </Stack>
+        </Box>
+    );
+};
+
+export default CharacterSheetBasicDetails;
--- client/src/schemas/characterSheetSchema.ts
+++ client/src/schemas/characterSheetSchema.ts
@@ -2,6 +2,18 @@ import { z } from "zod";
 
 export const ActorSheetDataSchema = z.object({
     name: z.string(),
+    img: z.string(),
+    system: z.object({}),
+    items: z.array(
+        z.object({
+            _id: z.string(),
+            name: z.string(),
+            type: z.union([z.literal("class"), z.literal("feat"), z.literal("spell")]),
+            system: z.object({
+                levels: z.optional(z.number()),
+            }),
+        })
+    ),
 });
 
 export type ActorSheetData = z.infer<typeof ActorSheetDataSchema>;
