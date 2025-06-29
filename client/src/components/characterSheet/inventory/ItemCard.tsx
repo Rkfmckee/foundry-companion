71c577c56c5441277b269733fa92f754c59ab080
Ryan McKeever <rkfmckee@gmail.com>
2025-06-29 17:19:21 +0100

    Add description and tooltips to item card

--- client/src/components/characterSheet/inventory/ItemCard.tsx
+++ client/src/components/characterSheet/inventory/ItemCard.tsx
@@ -1,55 +1,69 @@
+import FcTooltip from "@/components/FcTooltip";
 import { Item } from "@/schemas/actorSheetSchema";
 import { Avatar, AvatarGroup } from "@chakra-ui/react/avatar";
+import { Collapsible } from "@chakra-ui/react/collapsible";
 import { HStack } from "@chakra-ui/react/stack";
-import SheetTooltip from "../../SheetTooltip";
-import { Tooltip } from "@/components/ui/tooltip";
+import DOMPurify from "isomorphic-dompurify";
+
 interface ItemCardProps {
     item: Item;
 }
 
 const ItemCard = ({ item }: ItemCardProps) => {
     return (
-        <HStack className="bottom-border py-1" gap={3}>
-            <AvatarGroup size="xs">
-                <Avatar.Root>
-                    <Avatar.Image src={`${import.meta.env.VITE_FVTT_URL}/${item.img}`} />
-                </Avatar.Root>
-            </AvatarGroup>
-            <span className="flex-grow-1 crop-text">{item.name}</span>
+        <Collapsible.Root className="bottom-border py-1">
+            <HStack gap={3}>
+                <AvatarGroup size="xs">
+                    <Avatar.Root>
+                        <Avatar.Image src={`${import.meta.env.VITE_FVTT_URL}/${item.img}`} />
+                    </Avatar.Root>
+                </AvatarGroup>
+                <Collapsible.Trigger className="flex-grow-1 crop-text text-start">
+                    <span className="flex-grow-1 crop-text">{item.name}</span>
+                </Collapsible.Trigger>
 
-            {item.system.weight && item.system.weight.value > 0 && (
-                <HStack gap={1}>
-                    <span>
-                        {item.system.weight?.value}
-                        &nbsp;
-                        {item.system.weight?.units}
-                    </span>
-                    <i className="fa-solid fa-weight-hanging" />
-                </HStack>
-            )}
+                {item.system.weight && item.system.weight.value > 0 && (
+                    <FcTooltip text="Weight">
+                        <HStack gap={1}>
+                            <span>
+                                {item.system.weight?.value}
+                                &nbsp;
+                                {item.system.weight?.units}
+                            </span>
+                            <i className="fa-solid fa-weight-hanging" />
+                        </HStack>
+                    </FcTooltip>
+                )}
 
-            {item.system.quantity && (
-                <HStack gap={1}>
-                    <span>{item.system.quantity}</span>
-                    <i className="fa-solid fa-hashtag" />
-                </HStack>
-            )}
-            {item.system.uses && item.system.uses.max && (
-                <span>
-                    <span>{parseInt(item.system.uses?.max) - (item.system.uses?.spent ?? 0)}</span>/<span>{item.system.uses?.max ?? 0}</span>
-                </span>
-            )}
-            {item.system.attuned && (
-                <Tooltip content="Attuned" showArrow openDelay={0} closeDelay={0}>
-                    <i className="fa-solid fa-sun" />
-                </Tooltip>
-            )}
-            {item.system.equipped && (
-                <Tooltip content="Equipped" showArrow openDelay={0} closeDelay={0}>
-                    <i className="fa-solid fa-shield-halved" />
-                </Tooltip>
-            )}
-        </HStack>
+                {item.system.quantity && (
+                    <FcTooltip text="Quantity">
+                        <HStack gap={1}>
+                            <span>{item.system.quantity}</span>
+                            <i className="fa-solid fa-hashtag" />
+                        </HStack>
+                    </FcTooltip>
+                )}
+
+                {item.system.uses && item.system.uses.max && (
+                    <FcTooltip text="Limited uses">
+                        <span>{parseInt(item.system.uses?.max) - (item.system.uses?.spent ?? 0)}</span>/<span>{item.system.uses?.max ?? 0}</span>
+                    </FcTooltip>
+                )}
+                {item.system.attuned && (
+                    <FcTooltip text="Attuned">
+                        <i className="fa-solid fa-sun" />
+                    </FcTooltip>
+                )}
+                {item.system.equipped && (
+                    <FcTooltip text="Equipped">
+                        <i className="fa-solid fa-shield-halved" />
+                    </FcTooltip>
+                )}
+            </HStack>
+            <Collapsible.Content>
+                <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.system.description.value) }} />
+            </Collapsible.Content>
+        </Collapsible.Root>
     );
 };
 
